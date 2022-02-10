3781ef9b6b75149cc848e052e0971e55666a878e
ChunjunHu <huchunjun0725@gmail.com>
2022-02-10 00:25:13 -0500

    modified tests.py

--- DjangoAPI/apiApp/tests.py
+++ DjangoAPI/apiApp/tests.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 class WebsiteTests(TestCase):
     def test_view_url_exists_at_desired_location(self):
         resp = self.client.get('/healthz')
-        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(resp.status_code, 200)
