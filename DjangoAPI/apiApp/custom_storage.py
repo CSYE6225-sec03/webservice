0cfd972d8294c5610a0fc084d46d4638e72ac45e
ChunjunHu <huchunjun0725@gmail.com>
2022-03-11 18:49:20 -0500

    o

--- DjangoAPI/apiApp/custom_storage.py
+++ DjangoAPI/apiApp/custom_storage.py
@@ -2,4 +2,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    # open
+    # bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    bucket_name = 'you-bucket-name'
