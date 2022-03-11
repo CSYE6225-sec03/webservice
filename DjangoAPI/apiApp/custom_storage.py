3960109882bdd613ad5524985408f7eba642b20b
ChunjunHu <huchunjun0725@gmail.com>
2022-03-11 18:46:29 -0500

    fix

--- DjangoAPI/apiApp/custom_storage.py
+++ DjangoAPI/apiApp/custom_storage.py
@@ -1,4 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = 'csye6225.dev.chunjunhu.me'
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
