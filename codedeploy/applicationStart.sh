d37b2d08b13ad1e19aa3b6e4a95e7601276f7ef9
ChunjunHu <huchunjun0725@gmail.com>
2022-04-01 00:19:02 -0400

    333

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
+cd /home/ec2-user/webservice/webservice
+ls -a
 cd /home/ec2-user/webservice
 ls -a
-
 echo "522222222222222222222222"
 sudo pip3 install -r requirements.txt
 sudo pip3 install pymysql
